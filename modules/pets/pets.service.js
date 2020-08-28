8114aa19f7d2604589dcaef2576a2cb378a203a2
Minh Nguyen <nguyen.715@osu.edu>
2020-08-28 18:47:27 -0400

    wrote pets.router and pets.service

--- modules/pets/pets.service.js
+++ modules/pets/pets.service.js
@@ -17,9 +17,34 @@ store.dogs.forEach(dog => pets.dogs.enqueue(dog))
 module.exports = {
   get() {
     // Return the pets next in line to be adopted.
+    const nextPets = {
+      nextCat,
+      nextDog
+    };
+
+    nextPets.nextCat = pets.cats.show();
+    nextPets.nextDog = pets.dogs.show();
+
+    return nextPets;
+  },
+
+  getAllDogs() {
+    return pets.dogs.all();
+  },
+
+  getAllCats() {
+    return pets.cats.all();
   },
 
   dequeue(type) {
     // Remove a pet from the queue.
+    if (type === 'cat') {
+      return pets.cats.dequeue();
+    }
+    else if (type === 'dog') {
+      return pets.dogs.dequeue();
+    }
+    else
+      throw new Error('Pet type should be "cat" or "dog".');
   }
 }
